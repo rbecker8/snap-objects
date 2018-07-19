e5993dd56b3504c15f4f2628a698624aabcfff2b
rbecker8 <rbecker8@cnm.edu>
2018-07-19 10:07:16 -0600

    Added class with two state variables, constructor.

---
+++ Objects.php
@@ -0,0 +1,47 @@
+<?php
+
+class snapChallenge {
+	private $name;
+	private $age;
+
+
+
+	public function __construct(string $newName, $newAge) {
+		try{
+			$this->setName($newName);
+			$this->setAge($newAge);
+		} catch(\InvalidArgumentException | \RangeException | \Exception | \TypeError $exception) {
+			$exceptionType = get_class($exception);
+			throw (new $exceptionType($exception->getMessage(), 0, $exception));
+		}
+	}
+
+	public function getName() : string {
+		return ($this->name);
+	}
+
+	public function setName(string $newName) {
+		$newName = trim($newName);
+		$newName = filter_var($newName, FILTER_SANITIZE_STRING, FILTER_FLAG_NO_ENCODE_QUOTES);
+
+		if(empty($newName) === true) {
+			throw new \InvalidArgumentException("content is empty or insecure");
+		}
+		$this->name = $newName;
+	}
+
+	public function getAge() : int {
+		return ($this->age);
+	}
+
+	public function setAge(int $newAge) {
+		if($newAge < 0 ) {
+			throw new \RangeException("content is empty or insecure");
+		}
+	$this->age = $newAge;
+	}
+}
+
+$snapChallenge = new snapChallenge("Oh Snap!", 3383838);
+var_dump($snapChallenge);
+
